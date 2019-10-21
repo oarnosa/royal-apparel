ae2fcfb8566a41ad94e547f9bf040522ed59dade
Orlando Arnosa <oarnosa@gmail.com>
2019-10-21 14:59:26 -0400

    Create routes for conact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import ContactPage from './pages/contact/contact.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
@@ -47,6 +48,7 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route path="/contact" component={ContactPage} />
         </Switch>
       </div>
     );
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './contact.styles.scss';
+
+const ContactPage = () => <div>CONTACT PAGE</div>;
+
+export default ContactPage;
