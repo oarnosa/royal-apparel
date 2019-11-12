969f9bbc81b5d74046d7a64982340f632a90f87e
Orlando Arnosa <oarnosa@gmail.com>
2019-11-12 09:55:15 -0500

    Create collections-overview, implement shop and directory reducers

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { SelectCollections } from '../../redux/shop/shop.selectors';
+
+import './collections-overview.styles.scss';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: SelectCollections
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
