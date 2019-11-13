6e1838452d590d9e86338dbf50b8ff9a10c57a7b
Orlando Arnosa <oarnosa@gmail.com>
2019-11-13 10:05:02 -0500

    Normalize shop data into hash table

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,4 @@
-import { createSelector } from 'reselect';
-
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
+import { createSelector } from "reselect";
 
 const selectShop = state => state.shop;
 
@@ -18,8 +10,5 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections =>
-      collections.find(
-        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-      )
+    collections => collections[collectionUrlParam]
   );
