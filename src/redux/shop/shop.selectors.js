6c14e6fc6d285685b69447abe570835bb0bf7662
Orlando Arnosa <oarnosa@gmail.com>
2019-11-14 18:37:10 -0500

    Updated components with new redux flow for directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,10 +1,15 @@
-import { createSelector } from "reselect";
+import { createSelector } from 'reselect';
 
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectShop],
+  collections => Object.keys(collections).map(key => collections[key])
 );
 
 export const selectCollection = collectionUrlParam =>
