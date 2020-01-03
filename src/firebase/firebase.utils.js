48b199b76e7e125873fd9342170846f425c0d423
Orlando Arnosa <oarnosa@gmail.com>
2020-01-03 10:11:18 -0500

    Update firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: 'AIzaSyCqWnh8UT4plvXuL7HnQQtV4OsIjOVQPN4',
+  authDomain: 'crwn-db-153b4.firebaseapp.com',
+  databaseURL: 'https://crwn-db-153b4.firebaseio.com',
+  projectId: 'crwn-db-153b4',
+  storageBucket: 'crwn-db-153b4.appspot.com',
+  messagingSenderId: '675998441084',
+  appId: '1:675998441084:web:72a6bd7e8426196d8df3f9',
+  measurementId: 'G-FHTZ6EP4FY'
 };
 
 firebase.initializeApp(config);
