17f389ec9d8ec06e7b184c91b7016547c08418f7
Orlando Arnosa <oarnosa@gmail.com>
2020-01-28 16:47:14 -0500

    Overhaul UI and large styles refactor

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: 'https://i.ibb.co/xSNsZhq/hats.jpg',
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      title: 'watches',
+      imageUrl: 'https://i.ibb.co/Hx8rhFh/watches.jpg',
       id: 2,
-      linkUrl: 'shop/jackets'
+      linkUrl: 'shop/watches'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.ibb.co/2SK6X62/sneakers.jpg',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://i.ibb.co/Wt0Mgkz/womens.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/6m1B7v6/mens.jpg',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
