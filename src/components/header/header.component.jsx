86e633b6da9885b048f83489a12eff4224c9d399
Orlando Arnosa <oarnosa@gmail.com>
2020-01-30 10:03:29 -0500

    Style changes

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,23 +14,23 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    <div className='options'>
+      <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className="option" to="/shop">
+      <Link className='option' to='/shop'>
         CONTACT
       </Link>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
+        <div className='option' onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="/signin">
+        <Link className='option' to='/signin'>
           SIGN IN
         </Link>
       )}
